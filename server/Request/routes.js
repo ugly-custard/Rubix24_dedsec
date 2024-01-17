63a3a7a54c147202526b5adfa1fdf51235c004f2
Mustafa Akolawala <112169542+MustafaAkolawala@users.noreply.github.com>
2024-01-18 02:14:23 +0530

    feat: add seed data (#16)
    * Everything works again pt 2
    
    * chore: added seed data
    
    * Chore: remove redundant files
    
    * chore: fixing small issues

--- server/Request/routes.js
+++ server/Request/routes.js
@@ -1,9 +1,13 @@
-import * as controller from './controller.js';
-import { Router } from 'express';
+import * as controller from './controller.js'
+import { Router } from 'express'
 
-const router = Router();
+const router = Router()
 
-router.get('/', controller.getAllRequest);
+router.get('/', controller.getAllRequests)
+router.get('/:id', controller.getRequestById)
+router.post('/', controller.createRequests)
+router.put('/:id', controller.updateRequest)
+router.delete('/:id', controller.deleteRequest)
+router.put('/:id/status', controller.updateRequestStatus)
 
-
-export default router;
+export default router
