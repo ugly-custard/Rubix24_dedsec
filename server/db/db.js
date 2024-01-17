2c69534de4c942d1ec6b5074a42a57e2060fc40d
ugly-custard <himanshu.mishra1123@gmail.com>
2024-01-17 17:54:32 +0530

    knex migrations

--- server/db/db.js
+++ server/db/db.js
@@ -1,21 +1,5 @@
-import knex from "knex";
+import knex from 'knex'
+import knexfile from './knexfile.cjs'
 
-export default knex({
-    client: 'postgres',
-    connection: {
-        host: "db",
-        user: "username",
-        password: "password",
-        database: "username",
-    },
-    migration: {
-        directory: "./migration",
-    },
-    seeds: {
-        directory: "./seeds",
-    },
-    pool: {
-        min: 2,
-        max: 10,
-    },
-});
+const db = knex(knexfile.development)
+module.exports = db
