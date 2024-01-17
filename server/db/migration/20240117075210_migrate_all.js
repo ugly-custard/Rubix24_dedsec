fd2629cd2e03f4df99c42e6ad065ceb27984c822
ugly-custard <himanshu.mishra1123@gmail.com>
2024-01-17 19:41:54 +0530

    imma kms

--- server/db/migration/20240117075210_migrate_all.js
+++ server/db/migration/20240117075210_migrate_all.js
@@ -3,10 +3,11 @@ import db from '../db.js'
 const dropAndCreateTables = async () => {
   try {
     await db.schema.dropTableIfExists('issue_status')
-    await db.schema.dropTableIfExists('gp_head')
-    await db.schema.dropTableIfExists('ngo')
+    await db.schema.dropTableIfExists('users')
+    await db.schema.dropTableIfExists('ngos')
+    await db.schema.dropTableIfExists('requests')
 
-    await db.schema.withSchema('public').createTable('gp_head', (table) => {
+    await db.schema.withSchema('public').createTable('users', (table) => {
       table.uuid('gp_id').primary()
       table.string('village_name').notNullable()
       table.string('village_address').notNullable()
@@ -18,7 +19,7 @@ const dropAndCreateTables = async () => {
       table.timestamps(true, true)
     })
 
-    await db.schema.withSchema('public').createTable('ngo', (table) => {
+    await db.schema.withSchema('public').createTable('ngos', (table) => {
       table.uuid('ngo_id').primary()
       table.string('ngo_name').notNullable()
       table.string('ngo_type').notNullable()
@@ -31,15 +32,26 @@ const dropAndCreateTables = async () => {
       table.timestamps(true, true)
     })
 
-    await db.schema.withSchema('public').createTable('issue_status', (table) => {
-      table.uuid('issue_id').primary()
-      table.uuid('ngo_id').unique().notNullable()
+    await db.schema
+      .withSchema('public')
+      .createTable('issue_status', (table) => {
+        table.uuid('issue_id').primary()
+        table.uuid('ngo_id').unique().notNullable()
+        table.string('status').notNullable()
+        table.string('ngo_officer').notNullable()
+        table.foreign('ngo_id').references('ngos.ngo_id')
+        table.timestamps(true, true)
+      })
+
+    await db.schema.withSchema('public').createTable('requests', (table) => {
+      table.uuid('id').primary()
+      table.integer('n_people').notNullable()
+      table.string('village_address').notNullable().unique()
       table.string('status').notNullable()
-      table.string('ngo_officer').notNullable()
-      table.foreign('ngo_id').references('ngo.ngo_id')
+      table.string('username').notNullable()
+      table.foreign('village_address').references('users.village_address')
       table.timestamps(true, true)
     })
-
     console.log('Tables dropped and created successfully!')
     process.exit(0)
   } catch (err) {
