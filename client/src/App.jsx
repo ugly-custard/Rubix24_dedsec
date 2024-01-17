2deeece72f3ac1271649ac45f33f200973ad3598
Adnan Khan <93566182+AdnanKhan-ak47@users.noreply.github.com>
2024-01-18 01:51:38 +0530

    Login UI Issue is somewhat resolved (#15)
    * fixed small issue
    
    * encryption and JWT added in the backend authentication
    
    * Login Design somewhat resolved

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,9 +8,9 @@ function App() {
   const loggedIn = true;
 
   return (
-    <>
-      {/* <Login  /> */}
-      <Dashboard />
+    <> 
+      <Login  />
+      {/* <Dashboard /> */}
     </>
   )
 }
