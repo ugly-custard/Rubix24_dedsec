8195e4bcc36800301298db6a9eedc57c124d8da9
AdnanKhan-ak47 <adnankhan07666@gmail.com>
2024-01-19 00:35:44 +0530

    User id param added while inserting request

--- server/Request/controller.js
+++ server/Request/controller.js
@@ -1,5 +1,9 @@
 import db from '../db/db.js'
 import { Router } from 'express'
+import jwt from 'jsonwebtoken'
+
+const JWT_SECRET = "5f4b50a65065027be65580a99edcfdafcf432098e88b4bed93073db34bcb18d5"
+
 
 const router = Router()
 
@@ -25,12 +29,14 @@ export const getRequestById = async (req, res) => {
 }
 
 export const createRequests = async (req, res) => {
-  const { username, n_people, location, latitude, longitude } = req.body
+  const { username, n_people, location, latitude, longitude, authtoken } = req.body
 
+  const token = jwt.verify(authtoken, JWT_SECRET)
+  const user_id = token.user.user_id
 
   try {
     const requests = await db('requests')
-      .insert({ username: username, n_people: n_people, longitude: longitude, latitude: latitude, location: location })
+      .insert({ username: username, n_people: n_people, longitude: longitude, latitude: latitude, location: location, user_id: user_id })
       .returning('*')
     console.log(requests)
     res.status(201).json({status: 'success'})
