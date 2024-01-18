85f68990bada74c2773e1a9273be877865cdb42e
Himanshu Mishra <63342464+ugly-custard@users.noreply.github.com>
2024-01-19 04:06:25 +0530

    Feedback (#34)
    * ???
    
    * feedback css
    
    * verification page api works

--- client/src/pages/VerificationPage.jsx
+++ client/src/pages/VerificationPage.jsx
@@ -1,33 +1,41 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import WaterRequestVerify from '../components/WaterRequestVerify'
 
 function VerificationPage() {
-    const verificationId = "abcd1234"
-    return (
-        <div>
-            <h1>Verification Page</h1>
-            <p>Verification id: {verificationId}</p>
-            {/* need a similar table thing here */}
-            <WaterRequestVerify
-                location='1234 Main St'
-                userCount='5'
-                userName='John Doe'
-                status='Pending'
-            />
-            <WaterRequestVerify
-                location='1234 Main St'
-                userCount='5'
-                userName='John Doe'
-                status='Pending'
-            />
-            <WaterRequestVerify
-                location='1234 Main St'
-                userCount='5'
-                userName='John Doe'
-                status='Pending'
-            />
-        </div>
-    )
+  const [verificationId, setVerificationId] = useState('abcd1234')
+  const [waterRequests, setWaterRequests] = useState([])
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('http://localhost:5000/api/request')
+        const data = await response.json()
+        setWaterRequests(data)
+        console.log(waterRequests)
+      } catch (error) {
+        console.error('Error fetching data:', error)
+      }
+    }
+
+    fetchData()
+  }, [])
+
+  return (
+    <div>
+      <h1>Verification Page</h1>
+      <p>Verification id: {verificationId}</p>
+      {/* need a similar table thing here */}
+      {waterRequests.map((req) => (
+        <WaterRequestVerify
+          location={req.location}
+          userCount={req.n_people}
+          username={req.username}
+          status={req.status}
+          key={req.req_id}
+        />
+      ))}
+    </div>
+  )
 }
 
 export default VerificationPage
