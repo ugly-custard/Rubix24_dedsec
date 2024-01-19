bc5104efe150b3eb2e1bafe1aefbd67ff0abc9f7
smit jiwani <smitjiwani44@gmail.com>
2024-01-19 12:50:27 +0530

    css changes

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -13,6 +13,7 @@ const Navbar = () => {
     <nav>
       <h2>AquaConnect</h2>
       <div className="right">
+        <p>In case of Emergency contact: 011-24362705</p>
         <GoogleTranslateWidget />
         <button className="logout-btn" onClick={handleClick}>
           Log Out
